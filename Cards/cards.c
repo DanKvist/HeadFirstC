39ed4ef60532c6bbcd2d33b7fe46b528cec45304
Dan Thilderkvist <dan.thilderkvist@hotmail.com>
2019-08-17 23:26:56 +0200

    Fixed compile error (typo)

--- Cards/cards.c
+++ Cards/cards.c
@@ -12,7 +12,7 @@
 	 char card_name[3];
 	 puts("Enter the card_name: ");
 	 scanf("%2s", card_name);
-	 int val = 0
+	 int val = 0;
 	 if (card_name[0] == 'K') {
 		 val = 10;
 	 } else if (card_name[0] == 'Q') {
